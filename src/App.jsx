ce28a7a26046c8e07873dfcecb9e5bee66c0588b
Enzo Brincalepe <enzo.brincalepe@gmail.com>
2025-09-13 13:31:52 -0300

    finalizando a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,16 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Background from './pages/Background'
-import Navbar from './pages/Navbar'
+import Home from "./pages/Home";
 
 import Logo from "./assets/logoBranca.png"
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Depois de 1 segundo, troca para a tela principal
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center text-white">
-      {loading ? (
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 1.8 }}
-          className="flex flex-col items-center justify-center"
-        >
-          <img src={Logo} alt="Logo" className="w-32 h-32 mb-4" />
-        </motion.div>
-      ) : (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-         
-        >
-          <Navbar/>
-
-        </motion.div>
-      )}
-    </div>
-
-
-
+    <>
+      <Home/>
+    </>
   )
 }
 
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,11 +1,25 @@
-import { div, header, nav } from "motion/react-client"
+import Logo from "../assets/logoBranca.png"
+import { AiOutlineUser } from "react-icons/ai";
+import { BsYoutube } from "react-icons/bs";
 
-function Navbar(){
-
-    return(
-        <header className="bg-red-600">
-            <h1>Hello world</h1>
+function Navbar() {
+    return (
+        <header className="bg-[#9C0528] flex items-center justify-between px-8 p-8">
+            <ul className="flex items-center justify-center text-lg text-white font-bold gap-6 flex-1">
+                <li>NOTICIAS</li>
+                <li>CAMPEONATO</li>
+                <li><img src={Logo} alt="Logo" className="w-20" /></li>
+                <li>JOGAR</li>
+                <li>
+                    <a href="https://www.youtube.com/@passabola" target="_blank" className="flex items-center">
+                        <BsYoutube size={20} className="mr-1" /> YOUTUBE
+                    </a>
+                </li>
+            </ul>
+            <a href="#" className="ml-auto ">
+                <AiOutlineUser size={40} color="white" />
+            </a>
         </header>
     )
 }
-export default Navbar
+export default Navbar;
