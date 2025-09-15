335dd52a0950fee1e33330464e19a3ab9efb7d10
luccas <luccasfigueiragc@gmail.com>
2025-09-15 17:47:04 -0300

    Implement routing with react-router-dom and update Navbar for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PageCampeonato from "./pages/PageCampeonato";
 import PageNoticias from "./pages/PageNoticias";
 
 function App() {
-
   return (
-    <>
-      <Home/>
-      <PageCampeonato/>
-      <PageNoticias/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/campeonato" element={<PageCampeonato />} />
+        <Route path="/noticias" element={<PageNoticias />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import Logo from "../assets/logoBranca.png"
 import { AiOutlineUser } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logoBranca.png";
 
 function Navbar() {
     return (
         // bg-gradient-to-bl from-[#9C0528] to-[#7D001F] 
         <header className="flex items-center justify-between px-8 pt-12 drop-shadow-xl drop-shadow-[#9c0528d4]" >
             <ul className="flex items-center justify-center text-lg text-white font-bold gap-6 flex-1">
-                <li>NOTICIAS</li>
-                <li><a href="../pages/PageCampeonato">CAMPEONATO</a></li>
-                <li><img src={Logo} alt="Logo" className="w-20" /></li>
+                <li><Link to="/noticias">NOTICIAS</Link></li>
+                <li><Link to="/campeonato">CAMPEONATO</Link></li>
+                <li><Link to="/"><img src={Logo} alt="Logo" className="w-20" /></Link></li>
                 <li>JOGAR</li>
                 <li>
                     <a href="https://www.youtube.com/@passabola" target="_blank" className="flex items-center">
