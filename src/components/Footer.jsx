241ad0e538ec157d6368749d1abb17c82e26d44c
luccas <luccasfigueiragc@gmail.com>
2025-09-16 20:28:39 -0300

    redes ajustadas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,30 +12,30 @@ function Footer(){
                 </div>
                 <div className="mt-4 lg:mt-0">
                     <h1 className="flex items-center justify-center p-3 lg:p-5 text-lg sm:text-xl lg:text-2xl text-white font-semibold">Nossas redes</h1>
-                    <ul className="flex flex-row gap-2 sm:gap-3 lg:gap-4 flex-wrap justify-center">
-                        <li className="bg-white rounded-full p-2 hover:bg-gray-200 transition-colors">
-                            <a target="_blank" href="https://www.facebook.com/oficialpassaabola/?locale=pt_BR" className="block">
-                                <FaFacebook size={24} color="black" className="sm:w-8 sm:h-8 lg:w-10 lg:h-10"/>
+                    <ul className="flex flex-row gap-2 sm:gap-3 lg:gap-4 xl:gap-6 flex-wrap justify-center">
+                        <li className="bg-white rounded-full p-2 sm:p-3 lg:p-4 hover:bg-gray-200 transition-all duration-300 hover:scale-110">
+                            <a target="_blank" href="https://www.facebook.com/oficialpassaabola/?locale=pt_BR" className="block flex items-center justify-center">
+                                <FaFacebook className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10 xl:w-12 xl:h-12" color="black"/>
                             </a>
                         </li>
-                        <li className="bg-white rounded-full p-2 hover:bg-gray-200 transition-colors">
-                            <a target="_blank" href="https://www.youtube.com/@passabola" className="block">
-                                <BsYoutube size={24} color="black" className="sm:w-8 sm:h-8 lg:w-10 lg:h-10"/>
+                        <li className="bg-white rounded-full p-2 sm:p-3 lg:p-4 hover:bg-gray-200 transition-all duration-300 hover:scale-110">
+                            <a target="_blank" href="https://www.youtube.com/@passabola" className="block flex items-center justify-center">
+                                <BsYoutube className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10 xl:w-12 xl:h-12" color="black"/>
                             </a>
                         </li>
-                        <li className="bg-white rounded-full p-2 hover:bg-gray-200 transition-colors">
-                            <a target="_blank" href="https://www.instagram.com/passaabola/" className="block">
-                                <FaInstagram size={24} color="black" className="sm:w-8 sm:h-8 lg:w-10 lg:h-10"/>
+                        <li className="bg-white rounded-full p-2 sm:p-3 lg:p-4 hover:bg-gray-200 transition-all duration-300 hover:scale-110">
+                            <a target="_blank" href="https://www.instagram.com/passaabola/" className="block flex items-center justify-center">
+                                <FaInstagram className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10 xl:w-12 xl:h-12" color="black"/>
                             </a>
                         </li>
-                        <li className="bg-white rounded-full p-2 hover:bg-gray-200 transition-colors">
-                            <a target="_blank" href="https://www.tiktok.com/@passabola" className="block">
-                                <FaTiktok size={24} color="black" className="sm:w-8 sm:h-8 lg:w-10 lg:h-10"/>
+                        <li className="bg-white rounded-full p-2 sm:p-3 lg:p-4 hover:bg-gray-200 transition-all duration-300 hover:scale-110">
+                            <a target="_blank" href="https://www.tiktok.com/@passabola" className="block flex items-center justify-center">
+                                <FaTiktok className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10 xl:w-12 xl:h-12" color="black"/>
                             </a>
                         </li>
-                        <li className="bg-white rounded-full p-2 hover:bg-gray-200 transition-colors">
-                            <a target="_blank" href="https://x.com/passaabola" className="block">
-                                <BsTwitterX size={24} color="black" className="sm:w-8 sm:h-8 lg:w-10 lg:h-10"/>
+                        <li className="bg-white rounded-full p-2 sm:p-3 lg:p-4 hover:bg-gray-200 transition-all duration-300 hover:scale-110">
+                            <a target="_blank" href="https://x.com/passaabola" className="block flex items-center justify-center">
+                                <BsTwitterX className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10 xl:w-12 xl:h-12" color="black"/>
                             </a>
                         </li>
                     </ul>
