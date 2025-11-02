e3b40e060c8abd685eeb805e897efa506286658d
Enzo Brincalepe <enzo.brincalepe@gmail.com>
2025-11-02 15:48:06 -0300

    adcionando o titulo editavel

--- src/pages/pageAdmin/routes/championshipRoute/components/teste/TesteRoute.jsx
+++ src/pages/pageAdmin/routes/championshipRoute/components/teste/TesteRoute.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import edit from "../../assets/edit.png";
 import useButton from "../../store/state";
 import {Button, Box} from "@mui/material"
 function TesteRoute() {
   const { componentChange } = useButton();
     
+  const [name, setName] = useState("Campeonato PAB");
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEdit() {
+        setIsEditing(true);
+    }
+
+    function finishEditing() {
+        setIsEditing(false);
+     }
+
+     function handleKeyDown(e) {
+        if (e.key === "Enter") {
+            finishEditing();
+        }
+    }
   
   
   return (
@@ -24,7 +42,30 @@ function TesteRoute() {
         >
             Voltar
         </Button>
+            <div className="flex flex-col items-center m-10">
+        <div className="flex items-center justify-center">
+          {isEditing ? (
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              onBlur={finishEditing}
+              onKeyDown={handleKeyDown}
+              autoFocus
+              className="text-5xl font-bold text-[#D9D9D9] bg-transparent border-b border-gray-500 focus:outline-none px-8 text-center"
+            />
+          ) : (
+            <h1 className="text-5xl font-bold text-[#D9D9D9] px-8">
+              {name}
+            </h1>
+          )}
 
+          <button onClick={handleEdit}>
+            <img src={edit} alt="edit" className="w-[2.8rem]" />
+          </button>
+        </div>
+        <div className="bg-[#828282] w-1/2 h-[1px] mt-7"></div>
+      </div>
        
 
     </Box>
